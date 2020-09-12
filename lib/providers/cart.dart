b5f674a8849676730f4ab0331eb61b928a4c0417
Kalpesh Patel <kalpeshp@vmware.com>
2020-09-12 14:52:11 +0530

    Added multiple provider

---
+++ lib/providers/cart.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final double price;
+  final int quantity;
+
+  CartItem({
+    @required this.id,
+    @required this.title,
+    @required this.price,
+    @required this.quantity,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {...items};
+  }
+
+  void addItem(
+    String productId,
+    String title,
+    double price,
+  ) {
+    if (_items.containsKey(productId)) {
+      _items.update(productId, (CartItem existingCartItem) {
+        return CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity + 1,
+        );
+      });
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+            id: DateTime.now().toString(),
+            title: title,
+            price: price,
+            quantity: 1),
+      );
+    }
+  }
+}
